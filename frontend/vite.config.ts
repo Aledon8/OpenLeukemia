6a85388c775fcf91af544709351849a23c96de9d
Aledon8 <aledon.job@gmail.com>
2026-05-23 16:38:13 +0300

    fix(vite): delete mode

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,7 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-export default defineConfig(({ mode }) => ({
+export default defineConfig(() => ({
   base: "/",
   plugins: [react()],
   test: {
